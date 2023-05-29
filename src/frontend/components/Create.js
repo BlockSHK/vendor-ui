cabcced9669110131bbf69e27e0d8a7c3441fe6a
Hithru <hithrudealwis@gmail.com>
2023-05-29 16:43:04 +0530

    Update create form of the license

--- src/frontend/components/Create.js
+++ src/frontend/components/Create.js
@@ -1,33 +1,86 @@
 import React, { Component } from "react";
+import { Grid } from "@mui/material";
 import axios from "axios";
+import {
+  TextField,
+  Button,
+  Box,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  Typography,
+} from "@mui/material";
+
+const INFURA_ID = "2QSqx8RXM2BuNuWHMXV67dEfP08";
+const INFURA_SECRET_KEY = "041da3f3de9aff368b13e103f8fec4a7";
+
+const auth =
+  "Basic " +
+  Buffer.from(INFURA_ID + ":" + INFURA_SECRET_KEY).toString("base64");
 
 export default class Create extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      address: "",
+      company: "",
+      description: "",
+      image: "",
+      name: "",
+      owner: "",
+      price: "",
+      metadata: "",
+      software: "",
+      status: "ACTIVE",
+      type: "",
+      subscriptionPeriod: "",
+      paymentToken: "",
       response: null,
       error: null,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.uploadToIPFS = this.uploadToIPFS.bind(this);
   }
 
   handleChange(event) {
-    const inputValue = event.target.value;
-    const stateField = event.target.name;
     this.setState({
-      [stateField]: inputValue,
+      [event.target.name]: event.target.value,
     });
-    console.log(this.state);
   }
+
+  async uploadToIPFS(event) {
+    const file = event.target.files[0];
+    const data = new FormData();
+    data.append("file", file);
+    try {
+      const result = await axios.post(
+        "https://ipfs.infura.io:5001/api/v0/add",
+        data,
+        {
+          headers: {
+            "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+            Authorization: auth,
+          },
+        }
+      );
+      this.setState({
+        metadata: `https://ipfs.infura.io/ipfs/${result.data.Hash}`,
+      });
+    } catch (error) {
+      console.error("Upload to IPFS failed:", error);
+    }
+  }
+
   async handleSubmit(event) {
     event.preventDefault();
-    const { address } = this.state;
+    const data = { ...this.state };
+    delete data.response;
+    delete data.error;
     try {
       const response = await axios.post(
-        "https://b1r5aq31x2.execute-api.us-east-1.amazonaws.com/Prod/activation/nonce",
-        { address }
+        "https://b1r5aq31x2.execute-api.us-east-1.amazonaws.com/Prod/create",
+        data
       );
       this.setState({ response: response.data, error: null });
     } catch (error) {
@@ -36,27 +89,140 @@ export default class Create extends Component {
   }
 
   render() {
-    const { response, error } = this.state;
+    const { response, error, type } = this.state;
 
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>Address:</label>
-          <input
-            type="text"
-            name="address"
+      <Box
+        component="form"
+        onSubmit={this.handleSubmit}
+        noValidate
+        autoComplete="off"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1em",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+          width: "50%",
+          margin: "0 auto",
+          boxSizing: "border-box",
+          padding: "1em",
+          border: "1px solid #ddd",
+          borderRadius: "5px",
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create License
+        </Typography>
+        <TextField
+          label="Company"
+          name="company"
+          onChange={this.handleChange}
+          fullWidth
+        />
+        <TextField
+          label="Description"
+          name="description"
+          onChange={this.handleChange}
+          fullWidth
+        />
+        <TextField
+          label="Image URL"
+          name="image"
+          onChange={this.handleChange}
+          fullWidth
+        />
+        <TextField
+          label="Name"
+          name="name"
+          onChange={this.handleChange}
+          fullWidth
+        />
+        <TextField
+          label="Owner"
+          name="owner"
+          onChange={this.handleChange}
+          fullWidth
+        />
+        <TextField
+          label="Price"
+          name="price"
+          onChange={this.handleChange}
+          fullWidth
+        />
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              disabled
+              label="Metadata"
+              value={this.state.metadata}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <input
+              accept="*"
+              style={{ display: "none" }}
+              id="raised-button-file"
+              type="file"
+              onChange={this.uploadToIPFS}
+            />
+            <label htmlFor="raised-button-file">
+              <Button variant="contained" component="span">
+                Upload Metadata
+              </Button>
+            </label>
+          </Grid>
+        </Grid>
+        <TextField
+          label="Software"
+          name="software"
+          onChange={this.handleChange}
+          fullWidth
+        />
+        <FormControl fullWidth>
+          <InputLabel id="type-label">Type</InputLabel>
+          <Select
+            labelId="type-label"
+            value={type}
+            label="Type"
+            name="type"
             onChange={this.handleChange}
-            value={this.state.address}
+          >
+            <MenuItem value={"CONTRACT_PERPETUAL"}>Perpetual License</MenuItem>
+            <MenuItem value={"CONTRACT_FIXED_SUBSCRIPTION"}>
+              Fixed Subscription
+            </MenuItem>
+            <MenuItem value={"CONTRACT_AUTO_RENEW_SUBSCRIPTION"}>
+              Auto Renew Subscription
+            </MenuItem>
+          </Select>
+        </FormControl>
+        {(type === "CONTRACT_AUTO_RENEW_SUBSCRIPTION" ||
+          type === "CONTRACT_FIXED_SUBSCRIPTION") && (
+          <TextField
+            label="Subscription Period"
+            name="subscriptionPeriod"
+            onChange={this.handleChange}
+            fullWidth
           />
-          <button type="submit">Send</button>
-        </form>
+        )}
+        {type === "CONTRACT_AUTO_RENEW_SUBSCRIPTION" && (
+          <TextField
+            label="Payment Token"
+            name="paymentToken"
+            onChange={this.handleChange}
+            fullWidth
+          />
+        )}
+        <Button type="submit" variant="contained" color="primary">
+          Create
+        </Button>
         {response && (
           <div>
             <h2>Response:</h2>
-            <p>Status: {response.status}</p>
-            <p>Nonce: {response.payload.nonce}</p>
-            <p>Address: {response.payload.address}</p>
-            <p>Timestamp: {response.payload.timestamp}</p>
+            <p>{JSON.stringify(response, null, 2)}</p>
           </div>
         )}
         {error && (
@@ -65,7 +231,7 @@ export default class Create extends Component {
             <p>{error}</p>
           </div>
         )}
-      </div>
+      </Box>
     );
   }
 }
